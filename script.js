338bbcc9cfc7d7867b344ba4b9e468263b2a79a4
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-22 12:12:20 +0200

    ToDo list buttons and inputs are modified with css

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const dateInputElement = document.querySelector('.js-due-date')
 
 function toDoAdd() {
   const toDo = inputList.value;
-  const dateInput =dateInputElement.value;
+  const dateInput = dateInputElement.value;
   if (toDo) { // Check if the input is not empty
     toDoList.unshift({
       name: toDo,
@@ -25,12 +25,14 @@ function displayToDoList() {
     const name = toDoObject.name;
     const dueDate = toDoObject.dueDate;
     const html = `
-    <p>${name} ${dueDate}
-      <button onclick='
+    <div class = 'todo-text'>${name}</div>
+    <div class = 'todo-text'>${dueDate}</div>
+    <button class='delete-button' 
+      onclick='
         toDoList.splice(${i}, 1);
         displayToDoList()
       '>Delete</button>
-    </p>`;
+    `;
     listContainer.innerHTML += html;
   }
   console.log(toDoList);
